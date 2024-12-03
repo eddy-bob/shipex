fda89985edd4b6849fb0264d2ab1f6969bb01975
Madu Eddy <eddymadu90@gmail.com>
2024-12-03 12:36:53 +0100

    fix:updated the server env variable in a file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-
+import { SERVER_URL } from "./utils/static";
 module.exports = function (app) {
   app.use(
     "/v1",
     createProxyMiddleware({
-      target: "https://shippex-demo.bc.brandimic.com/",
+      target: SERVER_URL,
       changeOrigin: true,
       pathRewrite: { "^/v1": "" },
     })
